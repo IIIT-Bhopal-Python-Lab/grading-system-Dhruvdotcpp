c1494716466674f083040ba2a10730101503cc09
Dhruvdotcpp <dhruvwarwatkar@gmail.com>
2025-07-31 21:11:32 +0530

    Refactor grade output to simplify messages

--- lab1.py
+++ lab1.py
@@ -15,15 +15,15 @@ def get_grade():
         
         # Determine grade based on the marks entered
         if 0 <= marks < 40:
-            print("Grade: F")
+            print("F")
         elif 40 <= marks <= 59:
-            print("Grade: D")
+            print("D")
         elif 60 <= marks <= 74:
-            print("Grade: C")
+            print("C")
         elif 75 <= marks <= 89:
-            print("Grade: B")
+            print("B")
         elif 90 <= marks <= 100:
-            print("Grade: A")
+            print("A")
         else:
             print("Invalid marks! Please enter a number between 0 and 100.")
     
